83884dcae5c93aee4488adf45f1fc1cfc5f86b86
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2024-12-09 11:00:06 +0100

    Completed Day 4

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ https://adventofcode.com/2024
 # Progress
 - [X] Day 1
 - [X] Day 2
-- [ ] Day 3
-- [ ] Day 4
+- [X] Day 3
+- [X] Day 4
 - [ ] Day 5
 - [ ] Day 6
 - [ ] Day 7
---
+++ day04.py
@@ -0,0 +1,98 @@
+"""
+- Word search for the word "XMAS"
+- word can be horizontal, vertical, diagonal, written backwards or overlapping other words
+
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+
+"""
+
+word = "XMAS"
+directions = [
+    (0, 1),  # horizontal right
+    (0, -1),  # horizontal left
+    (1, 0),  # vertical down
+    (-1, 0),  # vertical up
+    (1, 1),  # diagonal down right
+    (1, -1),  # diagonal down left
+    (-1, 1),  # diagonal up right
+    (-1, -1)  # diagonal up left
+]
+
+
+def check_for_xmas(input_data, total_lines, total_cols, i, j, direction):
+    dir_x, dir_y = direction
+    # loop 4 steps (4 letters in XMAS) in each direction to check if it actually matches the word
+    for k, char in enumerate(word):
+        # check for the word
+        ii = i + k * dir_x
+        jj = j + k * dir_y
+        # check to not leave the grid
+        if not (0 <= ii < total_lines and 0 <= jj < total_cols):
+            return False
+        if input_data[ii][jj] != char:
+            return False
+    return True
+
+
+def check_for_mas_cross(input_data, total_lines, total_cols, i, j):
+    # check if the center is within the grid
+    if not (1 <= i < total_lines - 1 and 1 <= j < total_cols - 1):
+        return False
+    # center has to be an A
+    if input_data[i][j] != "A":
+        return False
+
+    # diagonal down right
+    diag_1 = f"{input_data[i-1][j-1]}{input_data[i+1][j+1]}"
+    # diagonal down left
+    diag_2 = f"{input_data[i-1][j+1]}{input_data[i+1][j-1]}"
+
+    # diagonal should be either MS or SM because they are reversable
+    if diag_1 in ["MS", "SM"] and diag_2 in ["MS", "SM"]:
+        return True
+
+    return False
+
+
+def part1(input_data):
+    solution = 0
+
+    total_lines = len(input_data)
+    total_cols = len(input_data[0])
+
+    # iterate through all locations and directions in the grid
+    for i in range(total_lines):
+        for j in range(total_cols):
+            for d in directions:
+                solution += check_for_xmas(input_data, total_lines, total_cols, i, j, d)
+
+    return solution
+
+
+def part2(input_data):
+    solution = 0
+
+    total_lines = len(input_data)
+    total_cols = len(input_data[0])
+
+    for i in range(total_lines):
+        for j in range(total_cols):
+            solution += check_for_mas_cross(input_data, total_lines, total_cols, i, j)
+
+    return solution
+
+
+if __name__ == "__main__":
+    with open("./inputs/day04.txt") as f:
+        data = f.read().strip().split("\n")
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
