6c5717c86a8ebaf29511d78aabb238241d93843a
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2024-12-10 09:52:05 +0100

    Added template file

---
+++ temp.py
@@ -0,0 +1,25 @@
+def part1(input_data):
+    solution = 0
+
+    lines = input_data.splitlines()
+
+    # sort the data into the two lists
+    #for line in lines:
+
+    return solution
+
+def part2(input_data):
+    solution = 0
+
+    lines = input_data.splitlines()
+
+    # sort the data into the two lists
+    # for line in lines:
+
+    return solution
+
+if __name__ == "__main__":
+    with open("./inputs/day01.txt") as f:
+        data = f.read().strip()
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
