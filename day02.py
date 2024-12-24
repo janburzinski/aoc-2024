577608f6f83150d17937c8e637ae8d670a559475
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2024-12-24 08:39:28 +0100

    Completed Day 2 Part 2

--- day02.py
+++ day02.py
@@ -12,6 +12,7 @@
 # a level can you be different by min. 1 and max. 3
 """
 
+
 def is_safe(report):
     only_increasing = None
 
@@ -28,17 +29,17 @@ def is_safe(report):
         # 1 3 5 => safe
         # 1 6 9 => unsafe
         if not check_adjacent_level_differ(nums[i], nums[i + 1]):
-            print(f"{nums[i]} has a too big difference between two numbers")
+            # print(f"{nums[i]} has a too big difference between two numbers")
             return False
 
-        # set the trend for the current report
+        # determine the trend for the current report
         if only_increasing is None:
             if diff > 0:
                 only_increasing = True
-                print(f"{nums[i]} is only increasing")
+                # print(f"{nums[i]} is only increasing")
             elif diff < 0:
                 only_increasing = False
-                print(f"{nums[i]} is only decreasing")
+                # print(f"{nums[i]} is only decreasing")
 
         # validate the trend
         if only_increasing and diff < 0:
@@ -48,27 +49,58 @@ def is_safe(report):
 
     return True
 
+
+# solves part 2
+def is_really_really_really_safe(report):
+    nums = list(map(int, report.split()))
+
+    # check if already safe
+    if is_safe(" ".join(map(str, nums))):
+        return True
+
+    # check by removing one number
+    for i in range(len(nums)):
+        modified_report = nums[:i] + nums[i + 1:]
+        if is_safe(" ".join(map(str, modified_report))):
+            return True
+    return False
+
+
 # the two numbers should only differ by a min. 1 and a max. 3
 def check_adjacent_level_differ(num, num2):
-    diff = abs(num - num2)
-    return 1 <= diff <= 3
+    return 1 <= abs(num - num2) <= 3
+
 
 def part1(input_data):
     solution = 0
 
-    # split the input data into separate reports to check each report individually
-    lines = input_data.splitlines()
-    print("line ", lines)
-
-    for line in lines:
-        print(f"check {line}")
+    for line in input_data:
         if is_safe(line):
-            print("safe ", line)
+            # print("safe ", line)
             solution += 1
 
     return solution
 
+
+"""
+part 2:
+- check if you can remove one number and make it safe that way
+"""
+
+
+def part2(input_data):
+    solution = 0
+
+    for line in input_data:
+        if is_really_really_really_safe(line):
+            # print("safe(pt2) ", line)
+            solution += 1
+
+    return solution
+
+
 if __name__ == "__main__":
     with open("./inputs/day02.txt") as f:
-         data = f.read().strip()
+        data = f.read().strip().split("\n")
     print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
